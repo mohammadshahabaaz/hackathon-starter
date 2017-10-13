b3d5c206232d90c5c716571944605b502e734820
mohammad <mohammadshahabaaz@gmail.com>
2017-10-14 02:50:46 +0530

    updatte and delete midway

--- public/js/main.js
+++ public/js/main.js
@@ -25,13 +25,9 @@ $(document).ready(function() {
   });
 
 
- /*   $('.button').click(function() {
-        $.ajax({
-            method: "POST",
-            url: "/deleteSport",
-            data: {name: $("#name").val(), type: $("#type").val()},
 
-        });
-    });*/
+
+
+
 
 });
